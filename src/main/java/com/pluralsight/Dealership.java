2c7bef6e7fd864fed90b7188f50ca2520554200c
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-05-17 13:53:54 -0500

    Second Commit

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
+import java.util.ArrayList;
+public class Dealership {// dealership name
+    private String name;
 
-public class Dealership {
+    // dealership address
+    private String address;
+
+    // dealership phone number
+    private String phone;
+
+    // list of vehicles in inventory
+    private ArrayList<Vehicle> inventory;
+
+    // constructor used to create a dealership object
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+
+// create the inventory list
+        inventory = new ArrayList<>();
+
+    }
 }
+
