78193880d246cf91dac4a30261cf859457d5b7b8
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-05-17 17:05:06 -0500

    Phase 2

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -17,8 +17,49 @@ public class DealershipFileManager {
 
             // read the dealership information line
             String dealershipLine = bufReader.readLine();
+
             // split the dealership line using pipe delimiters
             String[] dealershipParts = dealershipLine.split("\\|");
+
+            // create the dealership object using data from the file
+            dealership = new Dealership(
+                    dealershipParts[0],
+                    dealershipParts[1],
+                    dealershipParts[2]
+            );
+            // variable used to store each vehicle line
+            String input;
+
+            // loop through all remaining lines in the file
+            while ((input = bufReader.readLine()) != null) {
+                // split the vehicle line using pipe delimiters
+                String[] vehicleParts = input.split("\\|");
+
+                // convert the text data into proper data types
+                int vin = Integer.parseInt(vehicleParts[0]);
+                int year = Integer.parseInt(vehicleParts[1]);
+                String make = vehicleParts[2];
+                String model = vehicleParts[3];
+                String vehicleType = vehicleParts[4];
+                String color = vehicleParts[5];
+                int odometer = Integer.parseInt(vehicleParts[6]);
+                double price = Double.parseDouble(vehicleParts[7]);
+
+                // create a vehicle object using the parsed data
+                Vehicle vehicle = new Vehicle(
+                        vin,
+                        year,
+                        make,
+                        model,
+                        vehicleType,
+                        color,
+                        odometer,
+                        price
+                );
+                // add the vehicle to the dealership inventory
+                dealership.addVehicle(vehicle);
+            }
+
 
         } catch (IOException e) {
 
